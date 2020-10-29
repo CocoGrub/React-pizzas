3695bddfc24c473c2bab638574e832b20061a1c9
CocoGrub <fedsavin@gmail.com>
2020-10-30 02:35:34 +0300

    null prop check

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,7 +1,7 @@
 import React,{useState} from 'react'
 
 const Categories = ({data}) => {
-ca    const [current,setCurrent] = useState(null)
+ const [current,setCurrent] = useState(null)
     const clicked=(index)=>{
         setCurrent(index)
     }
@@ -10,7 +10,7 @@ ca    const [current,setCurrent] = useState(null)
             <div className="categories">
                 <ul>
                     <li className={current===null?'active':''} onClick={()=>clicked(null)}>Все</li>
-                  {data.map((name,index)=>{
+                  {data && data.map((name,index)=>{
                       return <li className={current===index?'active':''} onClick={()=>clicked(index)} key={name+index}>{name}</li>
                   })}
                 </ul>
