a2a0dbf3d5a0d01e1cb46211c91c93d23c7ad560
CocoGrub <fedsavin@gmail.com>
2020-10-31 03:45:28 +0300

    pizzas block render start

--- src/App.js
+++ src/App.js
@@ -7,19 +7,19 @@ import Home from './pages/Home';
 import Cart from './pages/Cart';
 
 function App() {
-  const [pizzas, setPizzas] = useState('');
+  const [pizzas, setPizzas] = useState([]);
   useEffect(() => {
     fetch('http://localhost:3000/db.json')
       .then((res) => res.json())
       .then((data) => setPizzas(data.pizzas));
   }, []);
-  console.log(pizzas);
+
   return (
     <div className="App">
       <div className="wrapper">
         <Header />
         <div className="content">
-          <Route exact path="/" component={Home} />
+          <Route exact path="/" render={() => <Home items={pizzas} />} />
           <Route exact path="/cart" component={Cart} />
         </div>
       </div>
